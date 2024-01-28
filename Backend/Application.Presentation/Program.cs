8a54b71af097076cd08beeb37f0746d1cf834d1d
Navid <navid0523@gmail.com>
2024-01-28 20:50:14 +0330

    add Infrastructure project

--- Backend/Application.Presentation/Program.cs
+++ Backend/Application.Presentation/Program.cs
@@ -9,18 +9,11 @@ namespace Application.Presentation
             // Add services to the container.
 
             builder.Services.AddControllers();
-
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
-
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
