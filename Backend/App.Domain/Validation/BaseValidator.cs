390df39dd840da1ae44783451b612b0d3c5ba6e1
Navid <navid0523@gmail.com>
2024-02-01 09:24:54 +0330

    add user validator

--- Backend/App.Domain/Validation/BaseValidator.cs
+++ Backend/App.Domain/Validation/BaseValidator.cs
@@ -37,5 +37,3 @@ namespace App.Domain.Validation
         public bool IsValid();
     }
 }
-
-
---
+++ Backend/App.Domain/Validation/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using App.Domain.Entities;
+
+namespace App.Domain.Validation
+{
+    public class UserValidator : BaseValidator<Entities.User>
+    {
+        public UserValidator(User model) : base(model)
+        {
+            if (model.Name.isnull)
+            {
+
+            }
+        }
+
+    }
+}
