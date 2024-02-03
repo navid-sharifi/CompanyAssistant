4bcbb0e3c301499aeb6bb13a619985069267871a
Navid <navid0523@gmail.com>
2024-02-03 13:56:08 +0330

    edit user services

--- Backend/App.Application/Services/UserServices.cs
+++ Backend/App.Application/Services/UserServices.cs
@@ -1,7 +1,17 @@
-﻿namespace App.Application.Services
+﻿using MediatR;
+
+namespace App.Application.Services
 {
     public class UserServices
     {
+        public UserServices(IMediator mediator)
+        {
+        }
 
+        public Task AddUser()
+        {
+
+        }
     }
+
 }
