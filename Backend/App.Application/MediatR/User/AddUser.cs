3ad570f043eb97db254ef4fa31f22ee1648a9488
Navid <navid0523@gmail.com>
2024-02-03 13:44:27 +0330

    AddUserRequest

---
+++ Backend/App.Application/MediatR/User/AddUser.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace App.Application.MediatR.User
+{
+    public class AddUserRequest : IRequest
+    {
+
+    }
+
+    public class AddUserHandler : IRequestHandler<AddUserRequest>
+    {
+        public Task Handle(AddUserRequest request, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+}
