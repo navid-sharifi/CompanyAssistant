6ebf74a4120b129a294f12948f4cfa9e8c3de2df
Navid <navid0523@gmail.com>
2024-02-03 14:11:46 +0330

    add AddApplication

---
+++ Backend/App.Application/AddApplication.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Application
+{
+    public static class AddApplicationExtention
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(typeof(AddApplicationExtention));
+
+            return services;
+        }
+
+    }
+}
