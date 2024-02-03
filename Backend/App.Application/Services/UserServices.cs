43017798662029b75b4223eca7e5f691ff0e2615
Navid <navid0523@gmail.com>
2024-02-03 14:03:14 +0330

    add  AddNewUserDto

--- Backend/App.Application/Services/UserServices.cs
+++ Backend/App.Application/Services/UserServices.cs
@@ -1,15 +1,22 @@
-﻿using MediatR;
+﻿using App.Application.MediatR.User;
+using App.Application.ViewModels.User.ViewModels;
+using MediatR;
 
 namespace App.Application.Services
 {
     public class UserServices
     {
+        private IMediator _mediator;
+
         public UserServices(IMediator mediator)
         {
+            _mediator = mediator;
         }
 
-        public Task AddUser()
+        public async Task AddUser(AddNewUserDto NewUserDto)
         {
+
+            await _mediator.Send(new AddUserRequest());
 
         }
     }
---
+++ Backend/App.Application/ViewModels/User/ViewModels/AddNewUserVM.cs
@@ -0,0 +1,7 @@
+﻿namespace App.Application.ViewModels.User.ViewModels
+{
+    public class AddNewUserDto
+    {
+
+    }
+}
