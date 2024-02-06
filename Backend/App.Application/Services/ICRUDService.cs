da5814ae465dbcc7c6e2b87335b0f7bcddf7e221
Navid <navid0523@gmail.com>
2024-02-06 15:04:53 +0330

    updare ICRud sevice

--- Backend/App.Application/Services/ICRUDService.cs
+++ Backend/App.Application/Services/ICRUDService.cs
@@ -1,12 +1,13 @@
 ﻿namespace App.Application.Services
 {
-    public interface ICRUDService<TAddDto>
+    public interface ICRUDService<TAddDto, TGet>
     {
         public Task Add(TAddDto add);
 
         public Task Delete(string id);
 
-        public Task Get(string id);
+        public TGet Get(string id);
 
+        public IList<TGet> GetAll(string id);
     }
 }
